ca74dcdc367ee599a257fcf43b56958c39d00941
Eero Kelly <eerokelly@gmail.com>
2022-06-15 19:33:34 -0700

    Add filter to incoming images

--- convex/fetchInbox.ts
+++ convex/fetchInbox.ts
@@ -2,6 +2,9 @@ import { query } from 'convex-dev/server';
 import { Image } from '../src/common';
 
 // List all incoming images.
-export default query(async ({ db }): Promise<Image[]> => {
-  return await db.table('images').collect(); // XXX filter based on signature
+export default query(async ({ db, signature }): Promise<Image[]> => {
+  return await db
+    .table('images')
+    .filter((q) => q.eq(q.field('dest_signature'), signature))
+    .collect();
 });
